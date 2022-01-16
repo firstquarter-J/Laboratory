48f80513ed57f045d24c9ad31efd7c9d0ecde4a2
Sahy <firstquarter88@gmail.com>
2022-01-16 20:36:23 +0900

    [feat] Clocl Component

--- README.md
+++ README.md
@@ -22,3 +22,17 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+### - feat : 새로운 기능 추가
+```
+- fix : 버그 수정
+- docs : 문서 수정
+- style : 코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
+- refactor : 코드 리팩토링
+- test : 테스트 코드, 리팩토링 테스트 코드 추가
+- chore : 빌드 업무 수정, 패키지 매니저 수정
+- 제목은 50자 미만, 문장의 끝에 마침표 넣지 않음. 과거 시제 사용하지 않고, 명령어로 작성하도록  함.
+- 제목 외에 추가적으로 정보를 전달하고 싶을 경우 본문에 추가 정보 기입
+- 예시 : [feat] comment router CRUD 기능 추가
+```
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img class="bgImage" src="../assets/cosmic.jpg" />
+    <p>{{ welcomeText }}</p>
+    <p>
+      <span class="red">난</span>
+      <span class="orange">장</span>
+      <span class="yellow">판</span>
+      <span class="green"> 실</span>
+      <span class="powderblue">험</span>
+      <span class="violet">실</span>
+    </p>
+    <!-- <HelloWorld msg="난장판 실험실" /> -->
+    <HelloWorld />
+    <Clock />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Vue from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Clock from "@/components/Clock.vue"; // @ is an alias to /src
 
 export default Vue.extend({
-  name: 'Home',
+  name: "Home",
   components: {
     HelloWorld,
+    Clock,
+  },
+  data() {
+    return {
+      welcomeText: "오리가 얼면? 언덕",
+    };
   },
 });
 </script>
+
+<style scoped>
+.bgImage {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  animation: fadeIn 0.5s linear;
+}
+span {
+  font-size: 88px;
+}
+.red {
+  color: rgb(214, 1, 1);
+}
+.orange {
+  color: rgb(218, 147, 17);
+}
+.yellow {
+  color: rgb(209, 209, 41);
+}
+.green {
+  color: rgb(19, 207, 19);
+}
+.powderblue {
+  color: rgb(10, 73, 192);
+}
+.violet {
+  color: rgb(128, 2, 128);
+}
+</style>
